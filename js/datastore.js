1787bfd2f06435d96e8ada8b5acfc324bb098204
sergey <sergey>
2018-06-05 16:27:47 +0300

    first commit

---
+++ js/datastore.js
@@ -0,0 +1,13 @@
+(function (window) {
+	'use strict';
+	
+	var App = window.App || {};
+
+	function DataStore() {
+		console.log('running the DataStore function');
+	}
+
+	App.DataStore = DataStore;
+	window.App = App;
+
+})(window);
